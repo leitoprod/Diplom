f1d5e7e971d81463b5a3590e2276488da92ab34f
Александр Печуркин <aapechurkin@edu.tversu.ru>
2019-09-18 00:28:48 +0300

    2.0 start version

---
+++ NS/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace NS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.День = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.data = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.d = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Прогноз = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.День,
+            this.data});
+            this.dataGridView1.Location = new System.Drawing.Point(833, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(152, 565);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // День
+            // 
+            this.День.HeaderText = "День";
+            this.День.Name = "День";
+            this.День.Width = 50;
+            // 
+            // data
+            // 
+            this.data.HeaderText = "Значения";
+            this.data.Name = "data";
+            this.data.Width = 80;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(48, 183);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(252, 53);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Подряд";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(48, 269);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(252, 53);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Случайно";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.d,
+            this.Прогноз});
+            this.dataGridView2.Location = new System.Drawing.Point(664, 12);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersVisible = false;
+            this.dataGridView2.Size = new System.Drawing.Size(163, 565);
+            this.dataGridView2.TabIndex = 4;
+            // 
+            // d
+            // 
+            this.d.HeaderText = "День";
+            this.d.Name = "d";
+            this.d.Width = 50;
+            // 
+            // Прогноз
+            // 
+            this.Прогноз.HeaderText = "Прогноз";
+            this.Прогноз.Name = "Прогноз";
+            this.Прогноз.Width = 80;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(73, 60);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(125, 40);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(291, 60);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(125, 40);
+            this.textBox2.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 24);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Xmin";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(230, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 24);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Xmax";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox3.Location = new System.Drawing.Point(276, 398);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(125, 40);
+            this.textBox3.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(997, 589);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn День;
+        private System.Windows.Forms.DataGridViewTextBoxColumn data;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn d;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Прогноз;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ NS/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace NS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Импортируем из txt данные в dataGridView
+            string s;
+            int i = 0;
+            StreamReader f = new StreamReader(@"C:\Users\Admin\source\repos\NS\NS\database.txt");
+            while ((s = f.ReadLine()) != null)
+            {
+                dataGridView1.Rows.Add();
+                dataGridView1.Rows[i].Cells[0].Value = i.ToString();
+                dataGridView1.Rows[i].Cells[1].Value = s.ToString();
+                i++;
+            }
+            f.Close();
+            dataGridView1.Rows.Add();
+            dataGridView1.Rows.Add();
+            int kolVoStrok = i;   //запоминаем количество строк импортированных из txt
+
+            //Находим Xmin
+            i = 0;
+            double xmin = double.Parse(dataGridView1.Rows[0].Cells[1].Value.ToString(), System.Globalization.CultureInfo.InvariantCulture);
+            while (i < kolVoStrok)
+            {
+                if (xmin > double.Parse(dataGridView1.Rows[i].Cells[1].Value.ToString(), System.Globalization.CultureInfo.InvariantCulture))
+                {
+                    xmin = double.Parse(dataGridView1.Rows[i].Cells[1].Value.ToString(), System.Globalization.CultureInfo.InvariantCulture);
+                }
+                i++;
+            }
+            textBox1.Text = xmin.ToString();
+
+
+            //Находим Xmax
+            i = 0;
+            double xmax = double.Parse(dataGridView1.Rows[0].Cells[1].Value.ToString(), System.Globalization.CultureInfo.InvariantCulture);
+            while (i<kolVoStrok)
+            {
+                if(xmax < double.Parse(dataGridView1.Rows[i].Cells[1].Value.ToString(), System.Globalization.CultureInfo.InvariantCulture))
+                {
+                    xmax = double.Parse(dataGridView1.Rows[i].Cells[1].Value.ToString(), System.Globalization.CultureInfo.InvariantCulture);
+                }
+                i++;
+            }
+            textBox2.Text = xmax.ToString();
+
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double v = 0.7;
+            double u = 0.05 ;
+            double a = 0.1;
+            double h0 = 0;
+            double W = 0.5;
+            double b = 0.1;
+            double h1, h2, h3, h4, h5, h6, h7, h8, h9, h10,hi;
+            double s1, s2, s3, s4, s5, s6, s7, s8, s9, s10;
+            double si = 0;
+            int i = 6;
+            double yres;
+
+
+            double maxMinusMin = double.Parse(textBox2.Text)-double.Parse(textBox1.Text);
+            s1 = (v * ((double.Parse(dataGridView1.Rows[5].Cells[1].Value.ToString(), System.Globalization.CultureInfo.InvariantCulture))-double.Parse(textBox1.Text))/maxMinusMin) + (u * (double.Parse(dataGridView1.Rows[4].Cells[1].Value.ToString(), System.Globalization.CultureInfo.InvariantCulture))) + a;
+            hi = s1;
+            while (i < 15)
+            {
+                si  = (v * ((double.Parse(dataGridView1.Rows[i].Cells[1].Value.ToString(), System.Globalization.CultureInfo.InvariantCulture)) - double.Parse(textBox1.Text)) / maxMinusMin) + (u * hi) + a;
+                h1 = si;
+                i++;
+
+
+
+            }
+            yres = W * si + b;
+            yres = yres * maxMinusMin + double.Parse(textBox1.Text);
+
+
+
+
+
+
+            textBox3.Text = yres.ToString();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = dataGridView1.Rows[0].Cells[1].Value.ToString();
+        }
+    }
+}
